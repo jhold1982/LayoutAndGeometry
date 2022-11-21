8a523b35fe1bcf7fa6b8c253e8ed4303734cd73d
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2022-11-21 14:49:20 -0600

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Justin Hold on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
